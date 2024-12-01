6d79510b031a356c5aa05d511e9f5613683c4983
MphoNthoesane <106187612+MphoNthoesane@users.noreply.github.com>
2024-12-01 13:42:48 +0200

    Add files via upload

---
+++ flight.js
@@ -0,0 +1,80 @@
+let btn1 = document.getElementById("btn1")
+let btn2 = document.getElementById("btn2")
+let btn3 = document.getElementById("btn3")
+let btn4 = document.getElementById("btn4")
+let btn5 = document.getElementById("btn5")
+let btn6 = document.getElementById("btn6")
+let btn7 = document.getElementById("btn7")
+let btn8 = document.getElementById("btn8")
+let btn9 = document.getElementById("btn9")
+let btn10 = document.getElementById("btn10")
+let btn11 = document.getElementById("btn11")
+let btn12 = document.getElementById("btn12")
+let btn13 = document.getElementById("btn13")
+let btn14 = document.getElementById("btn14")
+let btn15 = document.getElementById("btn15")
+
+
+
+
+document.getElementById("btn1").onclick = function(){
+    btn1.style.textDecoration = "line-through"
+    btn1.style.backgroundColor = "#696a6e"
+}
+document.getElementById("btn2").onclick = function(){
+    btn2.style.textDecoration = "line-through"
+    btn2.style.backgroundColor = "#696a6e"
+}
+document.getElementById("btn3").onclick = function(){
+    btn3.style.textDecoration = "line-through"
+    btn3.style.backgroundColor = "#696a6e"
+}
+
+document.getElementById("btn4").onclick = function(){
+    btn4.style.textDecoration = "line-through"
+    btn4.style.backgroundColor = "#696a6e"
+}
+document.getElementById("btn5").onclick = function(){
+    btn5.style.textDecoration = "line-through"
+    btn5.style.backgroundColor = "#696a6e"
+}
+document.getElementById("btn6").onclick = function(){
+    btn6.style.textDecoration = "line-through"
+    btn6.style.backgroundColor = "#696a6e"
+}
+document.getElementById("btn7").onclick = function(){
+    btn7.style.textDecoration = "line-through"
+    btn7.style.backgroundColor = "#696a6e"
+}
+document.getElementById("btn8").onclick = function(){
+    btn8.style.textDecoration = "line-through"
+    btn8.style.backgroundColor = "#696a6e"
+}
+document.getElementById("btn9").onclick = function(){
+    btn9.style.textDecoration = "line-through"
+    btn9.style.backgroundColor = "#696a6e"
+}
+document.getElementById("btn10").onclick = function(){
+    btn10.style.textDecoration = "line-through"
+    btn10.style.backgroundColor = "#696a6e"
+}
+document.getElementById("btn11").onclick = function(){
+    btn11.style.textDecoration = "line-through"
+    btn11.style.backgroundColor = "#696a6e"
+}
+document.getElementById("btn12").onclick = function(){
+    btn12.style.textDecoration = "line-through"
+    btn12.style.backgroundColor = "#696a6e"
+}
+document.getElementById("btn13").onclick = function(){
+    btn13.style.textDecoration = "line-through"
+    btn13.style.backgroundColor = "#696a6e"
+}
+document.getElementById("btn14").onclick = function(){
+    btn14.style.textDecoration = "line-through"
+    btn14.style.backgroundColor = "#696a6e"
+}
+document.getElementById("btn15").onclick = function(){
+    btn15.style.textDecoration = "line-through"
+    btn15.style.backgroundColor = "#696a6e"
+}
